d92166ed23e53503b7bbf8b5c2da5b173d3d0a38
Alex <60114079+Sashimee@users.noreply.github.com>
2020-03-24 11:56:47 +0100

    functions

--- src/app/services/server-request.service.ts
+++ src/app/services/server-request.service.ts
@@ -1,9 +1,28 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from "@angular/common/http";
 
 @Injectable({
   providedIn: 'root'
 })
 export class ServerRequestService {
+  path = '';
+  data = [];
+  baseUrl = 'localhost:3000'
+  constructor(private http: HttpClient) { }
 
-  constructor() { }
+  get(path: any) {
+    return this.http.get(this.baseUrl + path);
+  }
+
+  post(path: any, data: any) {
+    return this.http.post(this.baseUrl + path, data);
+  }
+
+  patch(path: any, data: any) {
+    return this.http.patch(this.baseUrl + path, data);
+  }
+
+  delete(path: any) {
+    return this.http.delete(this.baseUrl + path);
+  }
 }
