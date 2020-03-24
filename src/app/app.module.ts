a3e8f8ed54c3b606fe1df516cc8699661c750935
Alex <60114079+Sashimee@users.noreply.github.com>
2020-03-24 11:37:49 +0100

    copmponents

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { NewTaskComponent } from './components/new-task/new-task.component';
+import { NewListComponent } from './components/new-list/new-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    NewTaskComponent,
+    NewListComponent
   ],
   imports: [
     BrowserModule,
